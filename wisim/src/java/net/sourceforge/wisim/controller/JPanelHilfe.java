6adeede1d5a4ddffde4e24ff85ad7ac671d47587
admiral_kay <>
2003-05-28 21:47:09 +0000

    Modify the text of "Hilfe", add a disclaimer

--- wisim/src/java/net/sourceforge/wisim/controller/JPanelHilfe.java
+++ wisim/src/java/net/sourceforge/wisim/controller/JPanelHilfe.java
@@ -40,6 +40,7 @@ public class JPanelHilfe extends javax.swing.JPanel {
   /** Creates new form JPanelHilfe */
   public JPanelHilfe(WiSimMainController wiSimMainController) {
     initComponents();
+    modGui();
   }
 
 
@@ -55,17 +56,17 @@ public class JPanelHilfe extends javax.swing.JPanel {
         jLabelHilfe = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
 
-        jOptionPaneVerInfo.setMessageType(1);
+        jOptionPaneVerInfo.setMessageType(javax.swing.JOptionPane.INFORMATION_MESSAGE);
 
         setLayout(null);
 
         jTextArea1.setEditable(false);
         jTextArea1.setLineWrap(true);
-        jTextArea1.setText("Dieses Programm ist eine Simulation eines Waren-Wirtschaftssystems.\nFolgende Funktionen sind implementiert:\n\n    - Kundenverwaltung (Anlegen, Editieren, L\u00f6schen, \u00dcbersicht)\n    - Lieferantenverwaltung (Anlegen, Editieren, L\u00f6schen, \u00dcbersicht)\n    - Auftragsverwaltung (Anlegen, \u00dcbersicht)\n    - Arbeitsplatz- und Lager\u00fcbersicht mit Statusbalken\n    - Zahlungseingang verwalten\n    - Simulation der Produktion mit Auswertung\n\nChristian Drechsler\nDenise Freitag\nBenjamin Pasero\nKay Patzwald\nJanett Ziola\n\nDieses Programm ist ausschlie\u00dflich f\u00fcr\nBerufsschulzwecke gedacht und darf au\u00dferhalb \ndes OSZ KIM Berlin nur mit Genehmigung\nder Autoren eingesetzt werden.\nCopyright (c) 2003 Berlin - All Rights Reserved.\n");
+        jTextArea1.setText("Welcome to WiSim, a business game.\n\n\nVisit us at\n     http://wisim.sourceforge.net.\n\t\nVisit our forum for bugreports, feature requests, development or general dicussion.\n\nThe following topics were implemented in version 1.0: \n    - Single-User-Application \n    - simulation of production \n    - simulation of purchase \n    - simulation of sale \n    - analysis of the production / purchase / sale \n    - simulation of the store house \n    - customer and supplier administration\n\nIn Deutsch:\nDieses Programm ist ein Unternehmensplanspiel.\nFolgende Funktionen sind implementiert:\n\n    - Kundenverwaltung (Anlegen, Editieren, L\u00f6schen, \u00dcbersicht)\n    - Lieferantenverwaltung (Anlegen, Editieren, L\u00f6schen, \u00dcbersicht)\n    - Auftragsverwaltung (Anlegen, \u00dcbersicht)\n    - Arbeitsplatz- und Lager\u00fcbersicht mit Statusbalken\n    - Zahlungseingang verwalten\n    - Simulation der Produktion mit Auswertung\n\n\nCopyright notice                                                       \t\t\t\n\n(c) 2003 WiSim Development Team\nhttp://wisim.sourceforge.net/   \t\t\t                       \nAll rights reserved     \n                                  \nThis script is part of the WiSim Business Game project. The WiSim    \nproject is free software; you can redistribute it and/or modify        \nit under the terms of the GNU General Public License as published by   \nthe Free Software Foundation; either version 2 of the License, or      \n(at your option) any later version.                                    \n                                                                       \nThe GNU General Public License can be found at                         \nhttp://www.gnu.org/copyleft/gpl.html.                                  \nA copy is found in the textfile GPL.txt and important notices to the  \nlicense from the team is found in the textfile LICENSE.txt distributed \nin these package.                                                      \n\nSTAFF:\n------\n\n- Benjamin Pasero - Administrator and Developer\n- Kay Patzwald - Administrator and Developer\n");
         jScrollPaneHilfe.setViewportView(jTextArea1);
 
         add(jScrollPaneHilfe);
-        jScrollPaneHilfe.setBounds(20, 40, 750, 355);
+        jScrollPaneHilfe.setBounds(20, 40, 750, 851);
 
         jLabelHilfe.setFont(new java.awt.Font("Dialog", 1, 24));
         jLabelHilfe.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -85,6 +86,12 @@ public class JPanelHilfe extends javax.swing.JPanel {
 
     }//GEN-END:initComponents
 
+    /* Workaround for a possible bug in the Netbeans GUI-Editor*/
+    private void modGui()
+    {
+        jScrollPaneHilfe.setBounds(20, 40, 750, 355);
+    }
+    
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         JOptionPane.showMessageDialog(null, "Ver. 1.0");
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -94,8 +101,8 @@ public class JPanelHilfe extends javax.swing.JPanel {
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabelHilfe;
     private javax.swing.JOptionPane jOptionPaneVerInfo;
-    private javax.swing.JScrollPane jScrollPaneHilfe;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JScrollPane jScrollPaneHilfe;
     // End of variables declaration//GEN-END:variables
 
 }
