d79544a2d547a91c48ad0a2be7f0ca057abe03f9
admiral_kay <>
2003-06-25 18:36:19 +0000

    Interface SimulationPane for refresh panels

---
+++ wisim/src/java/net/sourceforge/wisim/model/SimulationPane.java
@@ -0,0 +1,15 @@
+/*
+ * Created on 25.06.2003
+ *
+ */
+package net.sourceforge.wisim.model;
+
+/**
+ * @author Kay Patzwald
+ *
+ */
+public interface SimulationPane {
+	
+	public void refresh();
+
+}
