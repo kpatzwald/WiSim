3695cca249e29c83ab7a3a3fa44a7218ed3881b0
admiral_kay <>
2003-06-30 16:36:06 +0000

    Implementation of JTextFieldValidation

--- wisim/src/java/net/sourceforge/wisim/model/Validator.java
+++ wisim/src/java/net/sourceforge/wisim/model/Validator.java
@@ -29,114 +29,147 @@
 
 package net.sourceforge.wisim.model;
 
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
 /** Verschiedene Methoden zum Validiern der User Eingabe in der Applikation
  * @author benjamin.pasero
  */
-public class Validator {
-    
-    /** Creates a new instance of Validate */
-    public Validator() {
-    }
-    
-    /** Überprüft ob die PLZ 5 Zeichen besitzt und nur aus Ziffern besteht.
-     * @param text Die PLZ als String
-     * @return TRUE wenn die PLZ stimmt
-     */
-    public boolean checkPlz(String text) {
-        String regExp = "^[0-9][1-9][0-9]{3}|[1-9][0-9][0-9]{3}$";
-        if (text.matches(regExp))
-            return true;
-        
-        return false;
-    }
-    
-    /** Überprüft die eingegebene email auf Gültigkeit.
-     * @param text Die email als String
-     * @return TRUE wenn die email korrekt ist
-     */
-    public boolean checkEMail(String text) {
-        String regExpEmail = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
-        int length = text.length();
-        if (length > 0) {
-            if (text.matches(regExpEmail))
-                return true;
-            
-            return false;
-        }
-        return false;
-    }
-    
-    /** Überprüft, ob ein korrekter Preis eingegeben wurde.
-     * @return boolean
-     * @param text Zu überprüfender Text.
-     */
-    public boolean checkPreis(String text) {
-        String regExp = "^([.|,][0-9]{1,2})|[0-9]{1,7}([.|,][0-9]{0,2})?$";
-        if (text.matches(regExp))
-            return true;
-        
-        return false;
-    }
-    
-    /** Überprüft, ob eine Zahl eingegeben wurde und das nicht mehr
-     * als 10 Ziffern eingegeben worden sind.
-     * @return boolean
-     * @param text Zu überprüfender Text.
-     */
-    public boolean checkZahl(String text) {
-        String regExp = "^([0-9]{0,10})$";
-        if (text.matches(regExp))
-            return true;
-        
-        return false;
-    }
-    
-    /** Erlaubt sind nur Eingaben von 1- oder 2-stellige Zahlen mit 1 Nachkommastelle
-     * getrennt durch einen Punkt
-     * @return boolean
-     * @param text Zu überprüfender Text.
-     */
-    public boolean checkPercent(String text) {
-        String regExp = "^([.|,][0-9]{1,2})|[0-9]{1,2}([.|,][0-9]{0,2})?$";
-        if (text.matches(regExp))
-            return true;
-        
-        return false;
-    }
-    
-    /** Erlaubt sind nur Eingaben von 4-stelligen Zahlen die nicht mit 0 beginnen
-     * @param text Zu überprüfender Text.
-     * @return boolean
-     */
-    public boolean checkYear(String text) {
-        String regExp = "^([1-9]{1}[0-9]{3})$";
-        if (text.matches(regExp))
-            return true;
-        
-        return false;
-    }
-    
-    /** Erlaubt sind nur Eingaben von 1- oder 2-stellige Zahlen
-     * @param text Zu überprüfender Text.
-     * @return boolean
-     */
-    public boolean checkTwoDigits(String text) {
-        String regExp = "^([0-9]{1,2})$";
-        if (text.matches(regExp))
-            return true;
-        
-        return false;
-    }
-    
-    /** Erlaubt sind nur Eingaben von 1- oder 2-stellige Zahlen
-     * @param text Zu überprüfender Text.
-     * @return boolean
-     */
-    public boolean checkThreeDigits(String text) {
-        String regExp = "^[1-9]{1}[0-9]{0,2}$";
-        if (text.matches(regExp))
-            return true;
-        
-        return false;        
-    }    
+public class Validator extends PlainDocument {
+	private int limit;
+
+	/** Creates a new instance of Validate */
+	public Validator() {
+	}
+
+	/**
+	 * Konstruktor für das Validationdokument
+	 * @param int limit: maximale Anzahl der einzugebenen Zeichen
+	 */
+	public Validator(int newLimit) {
+		super();
+		if (limit < 0) {
+			limit = 0;
+		} else {
+			limit = newLimit;
+		}
+	}
+
+	/**
+	   * Funktion überschreibt die Methode insertString von PlaintDocument
+	   * @param int offset: Position
+	   * @param String str: der String
+	   * @param AttributeSet attr: Attributset
+	   */
+	public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
+		if (str == null)
+			return;
+
+		if ((getLength() + str.length()) <= limit) {
+			super.insertString(offset, str, attr);
+		}
+	}
+
+	/** Überprüft ob die PLZ 5 Zeichen besitzt und nur aus Ziffern besteht.
+	 * @param text Die PLZ als String
+	 * @return TRUE wenn die PLZ stimmt
+	 */
+	public boolean checkPlz(String text) {
+		String regExp = "^[0-9][1-9][0-9]{3}|[1-9][0-9][0-9]{3}$";
+		if (text.matches(regExp))
+			return true;
+
+		return false;
+	}
+
+	/** Überprüft die eingegebene email auf Gültigkeit.
+	 * @param text Die email als String
+	 * @return TRUE wenn die email korrekt ist
+	 */
+	public boolean checkEMail(String text) {
+		String regExpEmail = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+		int length = text.length();
+		if (length > 0) {
+			if (text.matches(regExpEmail))
+				return true;
+
+			return false;
+		}
+		return false;
+	}
+
+	/** Überprüft, ob ein korrekter Preis eingegeben wurde.
+	 * @return boolean
+	 * @param text Zu überprüfender Text.
+	 */
+	public boolean checkPreis(String text) {
+		String regExp = "^([.|,][0-9]{1,2})|[0-9]{1,7}([.|,][0-9]{0,2})?$";
+		if (text.matches(regExp))
+			return true;
+
+		return false;
+	}
+
+	/** Überprüft, ob eine Zahl eingegeben wurde und das nicht mehr
+	 * als 10 Ziffern eingegeben worden sind.
+	 * @return boolean
+	 * @param text Zu überprüfender Text.
+	 */
+	public boolean checkZahl(String text) {
+		String regExp = "^([0-9]{0,10})$";
+		if (text.matches(regExp))
+			return true;
+
+		return false;
+	}
+
+	/** Erlaubt sind nur Eingaben von 1- oder 2-stellige Zahlen mit 1 Nachkommastelle
+	 * getrennt durch einen Punkt
+	 * @return boolean
+	 * @param text Zu überprüfender Text.
+	 */
+	public boolean checkPercent(String text) {
+		String regExp = "^([.|,][0-9]{1,2})|[0-9]{1,2}([.|,][0-9]{0,2})?$";
+		if (text.matches(regExp))
+			return true;
+
+		return false;
+	}
+
+	/** Erlaubt sind nur Eingaben von 4-stelligen Zahlen die nicht mit 0 beginnen
+	 * @param text Zu überprüfender Text.
+	 * @return boolean
+	 */
+	public boolean checkYear(String text) {
+		String regExp = "^([1-9]{1}[0-9]{3})$";
+		if (text.matches(regExp))
+			return true;
+
+		return false;
+	}
+
+	/** Erlaubt sind nur Eingaben von 1- oder 2-stellige Zahlen
+	 * @param text Zu überprüfender Text.
+	 * @return boolean
+	 */
+	public boolean checkTwoDigits(String text) {
+		String regExp = "^([0-9]{1,2})$";
+		if (text.matches(regExp))
+			return true;
+
+		return false;
+	}
+
+	/** Erlaubt sind nur Eingaben von 1- oder 2-stellige Zahlen
+	 * @param text Zu überprüfender Text.
+	 * @return boolean
+	 */
+	public boolean checkThreeDigits(String text) {
+		String regExp = "^[1-9]{1}[0-9]{0,2}$";
+		if (text.matches(regExp))
+			return true;
+
+		return false;
+	}
 }
