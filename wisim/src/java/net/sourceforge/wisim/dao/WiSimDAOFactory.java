345510dfa8e1a6edd506dc6cda589bdf54742a40
admiral_kay <>
2003-07-01 16:16:15 +0000

    Initialization of AuthentificationDAO

--- wisim/src/java/net/sourceforge/wisim/dao/WiSimDAOFactory.java
+++ wisim/src/java/net/sourceforge/wisim/dao/WiSimDAOFactory.java
@@ -34,21 +34,25 @@ package net.sourceforge.wisim.dao;
  * @author Kay Patzwald
  */
 public class WiSimDAOFactory {
-    
-    /** Creates a new instance of WiSimDAOFactory */
-    public WiSimDAOFactory() {
-    }
-    
-    /** returns a object with implements the <code>WiSimDAO</code> interface.
-     * @throws com.pixelpark.wisim.dao.WiSimDAOException if a database problem occurs
-     * @return the initialized DOA-object
-     */
-    public WiSimDAO getDAO() 
-        throws WiSimDAOException 
-    {
-    
-        WiSimDAOImpl daoI = new WiSimDAOImpl();
-        daoI.initialize();
-        return( (WiSimDAO) daoI);
-    }
+
+	/** Creates a new instance of WiSimDAOFactory */
+	public WiSimDAOFactory() {
+	}
+
+	/** returns a object with implements the <code>WiSimDAO</code> interface.
+	 * @throws com.pixelpark.wisim.dao.WiSimDAOException if a database problem occurs
+	 * @return the initialized DOA-object
+	 */
+	public WiSimDAO getDAO() throws WiSimDAOException {
+
+		WiSimDAOImpl daoI = new WiSimDAOImpl();
+		daoI.initialize();
+		return ((WiSimDAO) daoI);
+	}
+
+	public WiSimAuthentificationDAO getAuthDAO() throws WiSimDAOException {
+		WiSimDAOImpl dao = new WiSimDAOImpl();
+		dao.initialize();
+		return (WiSimAuthentificationDAO) dao;
+	}
 }
